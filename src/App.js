fa29ad2ab19baa11c31a8a62236371b41d90f5ba
anushka3002 <anushkasakshi2003@gmail.com>
2023-02-22 11:51:51 +0530

    mapped data and also implemented filter functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Products from './components/Products';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
+      <Products/>
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div></div>
-  )
-}
+    <div className="hero">
+      <div className="card text-bg-dark border-0">
+        <img src="/assets/hero-image.png" className="card-img" alt="..." />
+        <div className="card-img-overlay">
+          {/* <h5 className="card-title">Card title</h5> */}
+          <p className="card-text">
+            {/* This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer. */}
+          </p>
+          <p className="card-text">
+            {/* <small>Last updated 3 mins ago</small> */}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
